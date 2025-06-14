75d3b350e22263c0b89109d1c76bb896b85311f3
Kidus Petros <kidusptr@gmail.com>
2025-06-14 21:51:33 +0300

    creating migrations and managing dependencies

--- store/models.py
+++ store/models.py
@@ -22,6 +22,7 @@ class Promotion(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
     unit_price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.IntegerField()
