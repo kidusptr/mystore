7df54800159e07c773f2491c50db41af3fb62f2d
Kidus Petros <kidusptr@gmail.com>
2025-06-14 22:09:48 +0300

    looks like am getting somewhere with migrations

--- store/models.py
+++ store/models.py
@@ -74,6 +74,7 @@ class Order(models.Model):
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
+    zip = models.CharField(max_length=10, null=True)
     customer = models.OneToOneField(
         Customer, on_delete=models.CASCADE, primary_key=True
     )
