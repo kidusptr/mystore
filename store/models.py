e23e21b2c6c7be3675026344273b610990f15208
Kidus Petros <kidusptr@gmail.com>
2025-06-16 16:16:49 +0300

    naming conventions

--- store/models.py
+++ store/models.py
@@ -26,7 +26,7 @@ class Product(models.Model):
     description = models.TextField()
     unit_price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.IntegerField()
-    last_updated = models.DateTimeField(auto_now=True)
+    last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(Promotion)
 
