10950b0e85212ec5092e06a8692750f04abd7ba7
Kidus Petros <kidusptr@gmail.com>
2025-06-22 21:32:46 +0300

    better exception handling by showing off my HTML skills ha ha

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 
 
 def say_hello(request):
-
-    product = Product.objects.filter(pk=0).exists()
+    try:
+        product = Product.objects.get(pk=0)
+        print(product)
+    except ObjectDoesNotExist:
+        return render(request, "errors.html")
 
     return render(request, "main.html", {"name": "Django"})
 
