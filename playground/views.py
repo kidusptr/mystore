66bd408051abb395b353945a399cdbc7d86a3a4e
Kidus Petros <kidusptr@gmail.com>
2025-06-22 21:16:41 +0300

    getting started with retrving object and get to know managers and query_sets

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product
+
 
 def say_hello(request):
-    return render(request, 'main.html', {"name": "Django"})
+
+    product = Product.objects.filter(pk=0).exists()
+
+    return render(request, "main.html", {"name": "Django"})
+
+
 # Create your views here.
