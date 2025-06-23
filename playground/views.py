f8f5a7d73b4e6584d6b5a2286f06e60fbb412d4a
Kidus Petros <kidusptr@gmail.com>
2025-06-23 14:40:02 +0300

    field referencing using the F object

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Q
+from django.db.models import Q, F
 from store.models import Product
 
 
 def say_hello(request):
     try:
-        queryset = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
+        queryset = Product.objects.filter(inventory=F("collection_id"))
     except ObjectDoesNotExist:
         return render(request, "errors.html")
 
