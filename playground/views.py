6feb78584cbdf4ecdc37d4450c87dbdc4f1a6b30
Kidus Petros <kidusptr@gmail.com>
2025-06-23 14:34:22 +0300

    complex filtering with Q objects

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
 from store.models import Product
 
 
 def say_hello(request):
     try:
-        queryset = Product.objects.filter(title__icontains="coffee")
+        queryset = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
     except ObjectDoesNotExist:
         return render(request, "errors.html")
 
