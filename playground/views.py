fb5234dc4ea71b6f5f7175a63ffd7d3bccbd1976
Kidus Petros <kidusptr@gmail.com>
2025-06-24 13:18:55 +0300

    more on quering selected fields only

--- playground/views.py
+++ playground/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q, F
-from store.models import Product
+from store.models import Product, OrderItem
 
 
 def say_hello(request):
     try:
-        queryset = Product.objects.values("id", "title", "collection__title")[:20]
+        queryset = Product.objects.filter(
+            id__in=OrderItem.objects.values("product__id").distinct()
+        ).order_by("title")
 
     except ObjectDoesNotExist:
         return render(request, "errors.html")
