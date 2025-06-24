40128e26f289e65f9e641dd8981ba48b5f7abb77
Kidus Petros <kidusptr@gmail.com>
2025-06-24 22:04:33 +0300

    complex selecting fields

--- playground/views.py
+++ playground/views.py
@@ -2,22 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q, F
-from store.models import Product, OrderItem
+from store.models import Product, OrderItem, Order
 
 
 def say_hello(request):
     try:
         # queryset = Product.objects.defer("description")
+        # queryset = (
+        #     Product.objects.prefetch_related("promotions")
+        #     .select_related("collection")
+        #     .all()
+        # )
         queryset = (
-            Product.objects.prefetch_related("promotions")
-            .select_related("collection")
-            .all()
+            Order.objects.select_related("customer")
+            .prefetch_related("orderitem_set__product")
+            .order_by("-placed_at")[:5]
         )
-
     except ObjectDoesNotExist:
         return render(request, "errors.html")
 
-    return render(request, "main.html", {"name": "Django", "products": list(queryset)})
+    return render(request, "main.html", {"name": "Django", "orders": list(queryset)})
 
 
 # Create your views here.
