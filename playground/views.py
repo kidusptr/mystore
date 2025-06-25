fd1983ab3900d8f372a34ff86f3b7189c196a0d4
Kidus Petros <kidusptr@gmail.com>
2025-06-25 15:04:53 +0300

    annotating objects

--- playground/views.py
+++ playground/views.py
@@ -3,34 +3,16 @@ from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q, F
 from django.db.models.aggregates import Count, Sum, Avg, Max, Min
-from store.models import Product, OrderItem, Order
+from store.models import Product, OrderItem, Order, Customer
 
 
 def say_hello(request):
     try:
-        # queryset = Product.objects.defer("description")
-        # queryset = (
-        #     Product.objects.prefetch_related("promotions")
-        #     .select_related("collection")
-        #     .all()
-        # )
-        # queryset = (
-        #     Order.objects.select_related("customer")
-        #     .prefetch_related("orderitem_set__product")
-        #     .order_by("-placed_at")[:5]
-        # )
-
-        result = Product.objects.aggregate(
-            count=Count("id"),
-            min_price=Min("unit_price"),
-            max_price=Max("unit_price"),
-            avg_price=Avg("unit_price"),
-        )
-
+        queryset = Customer.objects.annotate(new_id=F("id") + 1)
     except ObjectDoesNotExist:
         return render(request, "errors.html")
 
-    return render(request, "main.html", {"name": "Django", "result": result})
+    return render(request, "main.html", {"name": "Django", "result": list(queryset)})
 
 
 # Create your views here.
