b1ada74402955a3fac384ff77581cb2b2bb79fdb
Kidus Petros <kidusptr@gmail.com>
2025-06-26 23:53:31 +0300

    generic relationshipd and custom managers

--- playground/views.py
+++ playground/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Count, ExpressionWrapper, F, DecimalField
 from store.models import Product, OrderItem, Order, Customer
+from tags.models import TaggedItem
 
 
 def say_hello(request):
     try:
-        discounted_price = ExpressionWrapper(
-            F("unit_price") * 0.8, output_field=DecimalField()
-        )
-        queryset = Product.objects.annotate(discount_price=discounted_price)
+
+        queryset = TaggedItem.objects.get_tags_for(Product, 1)
 
     except ObjectDoesNotExist:
         return render(request, "errors.html")
--- tags/models.py
+++ tags/models.py
@@ -4,11 +4,20 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 
 
 # Create your models here.
+class TaggedItemManager(models.Manager):
+    def get_tags_for(self, obj_type, obj_id):
+        content_type = ContentType.objects.get_for_model(obj_type)
+        return TaggedItem.objects.filter(
+            content_type=content_type, object_id=obj_id
+        ).select_related("tag")
+
+
 class Tag(models.Model):
     label = models.CharField(max_length=255)
 
 
 class TaggedItem(models.Model):
+    objects = TaggedItemManager()
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
