a17ee232a3f32f8555b9dd0979a4f75d32ca962d
Kidus Petros <kidusptr@gmail.com>
2025-06-27 00:24:12 +0300

    creating and updating objects

--- playground/views.py
+++ playground/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product, OrderItem, Order, Customer
+from store.models import Collection, Product
 from tags.models import TaggedItem
 
 
 def say_hello(request):
     try:
-
-        queryset = TaggedItem.objects.get_tags_for(Product, 1)
-
+        Collection.objects.filter(pk=12).update(title="Cosmetcis")
     except ObjectDoesNotExist:
         return render(request, "errors.html")
 
-    return render(request, "main.html", {"name": "Django", "result": list(queryset)})
+    return render(
+        request,
+        "main.html",
+        {"name": "Django"},
+    )
 
 
 # Create your views here.
