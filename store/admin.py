266f0258949f4305166e53df08265ae327b6555f
Kidus Petros <kidusptr@gmail.com>
2025-06-27 23:48:41 +0300

    implement searching and filtering our list pages

--- store/admin.py
+++ store/admin.py
@@ -7,12 +7,31 @@ from . import models
 # Register your models here.
 
 
+class InventoryFilter(admin.SimpleListFilter):
+    title = "Inventory"
+    parameter_name = "inventory"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("<10", "Low"),
+            (">=10", "OK"),
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == "<10":
+            return queryset.filter(inventory__lt=10)
+
+        if self.value() == ">=10":
+            return queryset.filter(inventory__gte=10)
+
+
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ["title", "unit_price", "inventory_status", "collection_title"]
     list_editable = ["unit_price"]
     list_per_page = 10
     list_select_related = ["collection"]
+    list_filter = ["collection", "last_update", InventoryFilter]
 
     @admin.display(ordering="inventory")
     def inventory_status(self, product):
@@ -29,7 +48,7 @@ class CustomerAdmin(admin.ModelAdmin):
     list_display = ["first_name", "last_name", "membership", "order_count"]
     list_editable = ["membership"]
     list_per_page = 10
-
+    search_fields = ["first_name__istartswith"]
     ordering = ["first_name", "last_name"]
 
     @admin.display(ordering="order_count")
