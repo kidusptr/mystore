738123f7a11238dd368dfbeba60e6f44b4679e3e
Kidus Petros <kidusptr@gmail.com>
2025-06-27 22:21:39 +0300

    adding computed columns

--- store/admin.py
+++ store/admin.py
@@ -9,11 +9,18 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = [
         "title",
         "unit_price",
+        "inventory_status",
     ]
     list_editable = [
         "unit_price",
     ]
     list_per_page = 10
+
+    @admin.display(ordering="inventory")
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return "Low"
+        return "OK"
 
 
 @admin.register(models.Customer)
