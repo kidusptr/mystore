292b7b4e68e74a938104f9e4b5477c671f99baed
Kidus Petros <kidusptr@gmail.com>
2025-06-27 22:39:25 +0300

    selecting related fields

--- store/admin.py
+++ store/admin.py
@@ -6,15 +6,10 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = [
-        "title",
-        "unit_price",
-        "inventory_status",
-    ]
-    list_editable = [
-        "unit_price",
-    ]
+    list_display = ["title", "unit_price", "inventory_status", "collection_title"]
+    list_editable = ["unit_price"]
     list_per_page = 10
+    list_select_related = ["collection"]
 
     @admin.display(ordering="inventory")
     def inventory_status(self, product):
@@ -22,20 +17,22 @@ class ProductAdmin(admin.ModelAdmin):
             return "Low"
         return "OK"
 
+    def collection_title(self, product):
+        return product.collection.title
+
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = [
-        "first_name",
-        "last_name",
-        "membership",
-    ]
-    list_editable = [
-        "membership",
-    ]
+    list_display = ["first_name", "last_name", "membership"]
+    list_editable = ["membership"]
     list_per_page = 10
 
     ordering = ["first_name", "last_name"]
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["id", "placed_at", "customer"]
 
 
 admin.site.register(models.Collection)
