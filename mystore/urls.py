592ea02a31aa39713e96e254d6cbd30697e7d709
Kidus Petros <kidusptr@gmail.com>
2025-06-27 21:52:43 +0300

    setting up the admin panel

--- mystore/urls.py
+++ mystore/urls.py
@@ -14,12 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
 
+admin.site.site_header = "MyStore Admin"
+admin.site.index_title = "MyStore Admin Portal"
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('playground/', include('playground.urls')),
+    path("admin/", admin.site.urls),
+    path("playground/", include("playground.urls")),
 ] + debug_toolbar_urls()
