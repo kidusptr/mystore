ae672877e4544e11cbd4bfc0294961dae4a5319c
Kidus Petros <kidusptr@gmail.com>
2025-06-28 21:48:19 +0300

    defining custom actions

--- store/admin.py
+++ store/admin.py
@@ -27,6 +27,7 @@ class InventoryFilter(admin.SimpleListFilter):
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
+    actions = ["clear_inventory"]
     list_display = ["title", "unit_price", "inventory_status", "collection_title"]
     list_editable = ["unit_price"]
     list_per_page = 10
@@ -41,6 +42,14 @@ class ProductAdmin(admin.ModelAdmin):
 
     def collection_title(self, product):
         return product.collection.title
+
+    @admin.action(description="Clear inventory")
+    def clear_inventory(self, request, queryset):
+        update_count = queryset.update(inventory=0)
+        self.message_user(
+            request,
+            f"{update_count} products were successfully updated.",
+        )
 
 
 @admin.register(models.Customer)
