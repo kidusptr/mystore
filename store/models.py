758144daef984fab2bd505e2dd8fc0d928c35313
Kidus Petros <kidusptr@gmail.com>
2025-06-29 22:13:52 +0300

    improvong form validations

--- store/models.py
+++ store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core import validators
 
 # Create your models here.
 
@@ -29,12 +30,14 @@ class Promotion(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
-    description = models.TextField()
-    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
-    inventory = models.IntegerField()
+    description = models.TextField(null=True, blank=True)
+    unit_price = models.DecimalField(
+        max_digits=10, decimal_places=2, validators=[validators.MinValueValidator(1)]
+    )
+    inventory = models.IntegerField(validators=[validators.MinValueValidator(0)])
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
-    promotions = models.ManyToManyField(Promotion)
+    promotions = models.ManyToManyField(Promotion, blank=True)
 
     def __str__(self):
         return self.title
