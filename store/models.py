c389e88e329b06c5bdccfecef8cdcba876083efa
Kidus Petros <kidusptr@gmail.com>
2025-07-01 22:58:46 +0300

    restore unecessary changes

--- store/models.py
+++ store/models.py
@@ -32,7 +32,7 @@ class Product(models.Model):
     slug = models.SlugField()
     description = models.TextField(null=True, blank=True)
     unit_price = models.DecimalField(
-        max_digits=10, decimal_places=2, validators=[validators.MinValueValidator(1)]
+        max_digits=6, decimal_places=2, validators=[validators.MinValueValidator(1)]
     )
     inventory = models.IntegerField(validators=[validators.MinValueValidator(0)])
     last_update = models.DateTimeField(auto_now=True)
