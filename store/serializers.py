301df6d5a73cb39e626ce14843feea957a6cc911
Kidus Petros <kidusptr@gmail.com>
2025-07-02 10:29:57 +0300

    creating a custom serializer fields

--- store/serializers.py
+++ store/serializers.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from rest_framework import serializers
 from .models import Product
 
@@ -5,8 +6,14 @@ from .models import Product
 class ProductSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
-    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
+    price = serializers.DecimalField(
+        max_digits=6, decimal_places=2, source="unit_price"
+    )
+    price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
+
+    def calculate_tax(self, product):
+        return product.unit_price + (product.unit_price * Decimal(0.15))
 
     class Meta:
         model = Product
-        fields = ["id", "title", "unit_price"]
+        fields = ["id", "title", "price", "price_with_tax"]
