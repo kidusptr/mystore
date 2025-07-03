583da1b6482edf3c09e843d1ce2f77df0032a79c
Kidus Petros <kidusptr@gmail.com>
2025-07-03 23:34:00 +0300

    CRUD for collections api

--- store/views.py
+++ store/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, Collection
+from .serializers import ProductSerializer, CollectionSerializer
+from django.db.models import Count
 
 # Create your views here.
 
@@ -45,6 +46,38 @@ def product_detail(request, id):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-@api_view()
+@api_view(["GET", "POST"])
+def collection_list(request):
+    collection = Collection.objects.annotate(products_count=Count("product")).all()
+    if request.method == "GET":
+        serializer = CollectionSerializer(collection, many=True)
+        return Response(serializer.data)
+
+    elif request.method == "POST":
+        serializer = CollectionSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(["GET", "PUT", "DELETE"])
 def collection_detail(request, id):
-    return Response("Ok")
+    collection = get_object_or_404(
+        Collection.objects.annotate(products_count=Count("product")), pk=id
+    )
+    if request.method == "GET":
+        serializer = CollectionSerializer(collection)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = CollectionSerializer(collection, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == "DELETE":
+        if collection.product_set.count() > 0:
+            return Response(
+                {"error": "Collection cannot be deleted because it has products"},
+                status=status.HTTP_405_METHOD_NOT_ALLOWED,
+            )
+        collection.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
