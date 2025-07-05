e67d42dd4468c17a76eb91b7db9d169c1f04665b
Kidus Petros <kidusptr@gmail.com>
2025-07-05 21:56:27 +0300

    viewsets

--- store/views.py
+++ store/views.py
@@ -2,51 +2,19 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.viewsets import ModelViewSet
 from django.db.models import Count
 from django.shortcuts import get_object_or_404
 from .models import Product, Collection
 from .serializers import ProductSerializer, CollectionSerializer
 
 
-# Create your views here.
-class ProductList(ListCreateAPIView):
-    queryset = Product.objects.select_related("collection").all()
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    # def get_queryset(self):
-    #     return Product.objects.select_related("collection").all()
-
-    # def get_serializer_class(self):
-    #     return ProductSerializer
 
     def get_serializer_context(self):
         return {"request": self.request}
-
-    # def get(self, request):
-    #     queryset = Product.objects.select_related("collection").all()
-    #     serializer = ProductSerializer(queryset, many=True)
-    #     return Response(serializer.data)
-
-    # def post(self, request):
-    #     serializer = ProductSerializer(data=request.data)
-    #     serializer.is_valid(raise_exception=True)
-    #     serializer.save()
-    #     return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
-class ProductDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    # def get(self, request, id):
-    #     product = get_object_or_404(Product, pk=id)
-    #     serializer = ProductSerializer(product, context={"request": request})
-    #     return Response(serializer.data)
-
-    # def put(self, request, id):
-    #     product = get_object_or_404(Product, pk=id)
-    #     serializer = ProductSerializer(product, data=request.data)
-    #     serializer.is_valid(raise_exception=True)
-    #     serializer.save()
-    #     return Response(serializer.data, status=status.HTTP_200_OK)
 
     def delete(self, request, pk):
         product = get_object_or_404(Product, pk=pk)
@@ -59,23 +27,8 @@ class ProductDetail(RetrieveUpdateDestroyAPIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class CollectionList(ListCreateAPIView):
-    queryset = Collection.objects.annotate(products_count=Count("product")).all()
-    serializer_class = CollectionSerializer
-
-    # def get(self, request):
-    #     queryset = Collection.objects.annotate(products_count=Count("product")).all()
-    #     serializer = CollectionSerializer(queryset, many=True)
-    #     return Response(serializer.data)
-
-    # def post(self, request):
-    #     serializer = CollectionSerializer(data=request.data)
-    #     serializer.is_valid(raise_exception=True)
-    #     serializer.save()
-    #     return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
-class CollectionDetail(RetrieveUpdateDestroyAPIView):
+# Create your views here.
+class CollectionViewSet(APIView):
     queryset = Collection.objects.annotate(products_count=Count("product")).all()
     serializer_class = CollectionSerializer
 
@@ -90,19 +43,3 @@ class CollectionDetail(RetrieveUpdateDestroyAPIView):
             )
         collection.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-    # def get(self, request, id):
-    #     collection = get_object_or_404(
-    #         Collection.objects.annotate(products_count=Count("product")), pk=id
-    #     )
-    #     serializer = CollectionSerializer(collection)
-    #     return Response(serializer.data)
-
-    # def put(self, request, id):
-    #     collection = get_object_or_404(
-    #         Collection.objects.annotate(products_count=Count("product")), pk=id
-    #     )
-    #     serializer = CollectionSerializer(collection, data=request.data)
-    #     serializer.is_valid(raise_exception=True)
-    #     serializer.save()
-    #     return Response(serializer.data, status=status.HTTP_200_OK)
