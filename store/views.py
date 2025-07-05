72932d46f83f460f51cac91e3a2770caa9232b37
Kidus Petros <kidusptr@gmail.com>
2025-07-05 22:35:15 +0300

    setting up  default router

--- store/views.py
+++ store/views.py
@@ -5,7 +5,7 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from rest_framework.viewsets import ModelViewSet
 from django.db.models import Count
 from django.shortcuts import get_object_or_404
-from .models import Product, Collection
+from .models import Product, Collection, OrderItem
 from .serializers import ProductSerializer, CollectionSerializer
 
 
@@ -16,30 +16,24 @@ class ProductViewSet(ModelViewSet):
     def get_serializer_context(self):
         return {"request": self.request}
 
-    def delete(self, request, pk):
-        product = get_object_or_404(Product, pk=pk)
-        if product.orderitem_set.count() > 0:
+    def destroy(self, request, *args, **kwargs):
+        if OrderItem.objects.filter(product_id=kwargs["pk"]).exists():
             return Response(
                 {"error": "Product cannot be deleted because it has an active order"},
                 status=status.HTTP_405_METHOD_NOT_ALLOWED,
             )
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request, *args, **kwargs)
 
 
 # Create your views here.
-class CollectionViewSet(APIView):
+class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(products_count=Count("product")).all()
     serializer_class = CollectionSerializer
 
-    def delete(self, request, pk):
-        collection = get_object_or_404(
-            Collection.objects.annotate(products_count=Count("product")), pk=pk
-        )
-        if collection.product_set.count() > 0:
+    def destroy(self, request, *args, **kwargs):
+        if Product.objects.filter(collection_id=kwargs["pk"]).exists():
             return Response(
                 {"error": "Collection cannot be deleted because it has products"},
                 status=status.HTTP_405_METHOD_NOT_ALLOWED,
             )
-        collection.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request, *args, **kwargs)
