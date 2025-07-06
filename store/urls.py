ada734f37a9d8b96bd3c72e5a07e712272a132cb
Kidus Petros <kidusptr@gmail.com>
2025-07-06 23:13:54 +0300

    Nested routers with handleing url params

--- store/urls.py
+++ store/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from . import views
-from rest_framework.routers import SimpleRouter
-from pprint import pprint
+from rest_framework_nested import routers
 
-router = SimpleRouter()
-
+router = routers.DefaultRouter()
 router.register("products", views.ProductViewSet)
 router.register("collections", views.CollectionViewSet)
 
-pprint(router.urls)
 
-urlpatterns = router.urls
+product_router = routers.NestedDefaultRouter(router, "products", lookup="product")
+product_router.register("reviews", views.ReviewViewSet, basename="product-reviews")
+
+
+urlpatterns = router.urls + product_router.urls
