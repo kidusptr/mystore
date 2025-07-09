65fa8224dc1b6d6346b43289f8764a85787f0975
Kidus Petros <kidusptr@gmail.com>
2025-07-09 16:08:10 +0300

    working on cart api

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core import validators
+from uuid import uuid4
 
 # Create your models here.
 
@@ -109,13 +110,17 @@ class OrderItem(models.Model):
 
 
 class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField()
+
+    class Meta:
+        unique_together = [["cart", "product"]]
 
 
 class Review(models.Model):
