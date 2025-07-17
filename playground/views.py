1a30951fedc05ce136491a19cbec9eee8af8823a
Kidus Petros <kidusptr@gmail.com>
2025-07-17 23:58:55 +0300

    i hope am not gonna use playgground anymore

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,1 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.core.exceptions import ObjectDoesNotExist
-from store.models import Collection, Product
-from tags.models import TaggedItem
-
-
-# Create your views here.
