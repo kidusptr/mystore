8339ac42a10833cf504a721b965f37a35e60ff19
Kidus Petros <kidusptr@gmail.com>
2025-07-17 23:58:19 +0300

    clean up

--- playground/views.py
+++ playground/views.py
@@ -5,18 +5,4 @@ from store.models import Collection, Product
 from tags.models import TaggedItem
 
 
-def say_hello(request):
-    try:
-        Collection.objects.filter(pk=12).delete()
-        queryset = Collection.objects.all()
-    except ObjectDoesNotExist:
-        return render(request, "errors.html")
-
-    return render(
-        request,
-        "main.html",
-        {"name": "Django", "collections": list(queryset)},
-    )
-
-
 # Create your views here.
