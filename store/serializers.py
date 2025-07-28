a3ff7e321a4697c4520f9a7980057587de376c16
Kidus Petros <kidusptr@gmail.com>
2025-07-28 22:45:31 +0300

    creating order items from cart items

--- store/serializers.py
+++ store/serializers.py
@@ -11,6 +11,8 @@ from .models import (
     OrderItem,
 )
 
+from django.db import transaction
+
 
 class CollectionSerializer(serializers.ModelSerializer):
     class Meta:
@@ -148,10 +150,27 @@ class CreateOrderSerializer(serializers.Serializer):
     cart_id = serializers.UUIDField()
 
     def save(self, **kwargs):
-        print(self.validated_data["cart_id"])
-        print(self.context["user_id"])
+        with transaction.atomic():
+            print(self.validated_data["cart_id"])
+            print(self.context["user_id"])
 
-        (customer, created) = Customer.objects.get_or_create(
-            user_id=self.context["user_id"]
-        )
-        Order.objects.create(customer=customer)
+            (customer, created) = Customer.objects.get_or_create(
+                user_id=self.context["user_id"]
+            )
+            order = Order.objects.create(customer=customer)
+
+            cart_items = CartItem.objects.select_related("product").filter(
+                cart_id=self.validated_data["cart_id"]
+            )
+            order_items = [
+                OrderItem(
+                    order=order,
+                    product=item.product,
+                    quantity=item.quantity,
+                    unit_price=item.product.unit_price,
+                )
+                for item in cart_items
+            ]
+            OrderItem.objects.bulk_create(order_items)
+            Cart.objects.filter(pk=self.validated_data["cart_id"]).delete()
+            return order
