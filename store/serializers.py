16876d24b85d2cae679677ddc516da4b9a162951
Kidus Petros <kidusptr@gmail.com>
2025-07-30 16:38:17 +0300

    impelmenting custom signals

--- store/serializers.py
+++ store/serializers.py
@@ -12,6 +12,7 @@ from .models import (
 )
 
 from django.db import transaction
+from .signals import order_created
 
 
 class CollectionSerializer(serializers.ModelSerializer):
@@ -178,6 +179,7 @@ class CreateOrderSerializer(serializers.Serializer):
             ]
             OrderItem.objects.bulk_create(order_items)
             Cart.objects.filter(pk=self.validated_data["cart_id"]).delete()
+            order_created.send_robust(self.__class__, order=order)
             return order
 
 
