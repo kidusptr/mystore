eed74897eb584c2f2a8e0dfaa07b18f35cb4b0c0
Kidus Petros <kidusptr@gmail.com>
2025-07-31 18:11:21 +0300

    validating image files when uploading

--- store/models.py
+++ store/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.core import validators
 from uuid import uuid4
 from django.conf import settings
+from .validators import validate_file_size
 
 # Create your models here.
 
@@ -52,7 +53,9 @@ class ProductImage(models.Model):
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE, related_name="images"
     )
-    image = models.ImageField(upload_to="store/images", null=True)
+    image = models.ImageField(
+        upload_to="store/images", null=True, validators=[validate_file_size]
+    )
 
 
 class Customer(models.Model):
