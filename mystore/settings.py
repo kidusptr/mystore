ae4ed59afc2c2c3eff24698836d11951fb42923f
Kidus Petros <kidusptr@gmail.com>
2025-08-01 21:56:17 +0300

    sending test email

--- mystore/settings.py
+++ mystore/settings.py
@@ -180,7 +180,7 @@ DJOSER = {
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "localhost"
-EMAIL_PORT = 25
+EMAIL_PORT = 2525
 EMAIL_HOST_USER = ""
 EMAIL_HOST_PASSWORD = ""
 DEFAULT_FROM_EMAIL = "kidus@localhost.com"
--- playground/views.py
+++ playground/views.py
@@ -1,1 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.core.mail import send_mail, mail_admins, BadHeaderError
+
+
+def index(request):
+    try:
+        send_mail(
+            "subject",
+            "message",
+            "kidus@localhost.com",
+            ["test@localhost.com"],
+        )
+    except BadHeaderError:
+        return HttpResponse("Invalid header found.")
+
+    return render(request, "main.html")
