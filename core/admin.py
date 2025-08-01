ebec732e23736b2797a7d093b75a7b29609e46d5
Kidus Petros <kidusptr@gmail.com>
2025-08-01 18:30:15 +0300

    showing image thumbnails for product on the admin site

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.contenttypes.admin import GenericTabularInline
-from store.admin import ProductAdmin
+from store.admin import ProductAdmin, ProductImageInline
 from store.models import Product
 from tags.models import TaggedItem
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
@@ -35,7 +35,7 @@ class TagInline(GenericTabularInline):
 
 
 class CustomProductAdmin(ProductAdmin):
-    inlines = [TagInline]
+    inlines = [TagInline, ProductImageInline]
 
 
 admin.site.unregister(Product)
