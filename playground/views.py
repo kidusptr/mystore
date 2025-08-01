3728f728726a46e423988f73fe707cda2d912b38
Kidus Petros <kidusptr@gmail.com>
2025-08-01 22:24:25 +0300

    attaching file to emails

--- playground/views.py
+++ playground/views.py
@@ -1,20 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.core.mail import mail_admins, BadHeaderError
+from django.core.mail import EmailMessage, BadHeaderError
 import os
 
 
 def index(request):
     try:
-        html_path = os.path.join(os.path.dirname(__file__), "email.html")
-        with open(html_path, "r") as f:
-            html_content = f.read()
-
-        mail_admins(
-            subject="Test Email from Django",
-            message="This is a fallback plain-text message.",
-            html_message=html_content,
+        message = EmailMessage(
+            "Subject",
+            "Message",
+            "kidus@localhost.com",
+            ["test@localhost.com"],
         )
+        message.attach_file(os.path.join("playground", "email.html"))
+        message.send()
         return HttpResponse("Test email sent to admins.")
 
     except BadHeaderError:
