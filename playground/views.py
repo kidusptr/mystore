a56cb1c089e04f0e144e8615bc41e1a24ac70764
kidusptr <kidusptr@gmail.com>
2025-08-03 17:40:30 +0300

    Running background tasks with celery

--- playground/views.py
+++ playground/views.py
@@ -1,22 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.core.mail import EmailMessage, BadHeaderError
+from .tasks import notify_customers
 import os
 
 
 def index(request):
-    try:
-        message = EmailMessage(
-            "Subject",
-            "Message",
-            "kidus@localhost.com",
-            ["test@localhost.com"],
-        )
-        message.attach_file(os.path.join("playground", "templates/emails/email.html"))
-        message.send()
-        return HttpResponse("Test email sent to admins.")
-
-    except BadHeaderError:
-        return HttpResponse("Invalid header found.")
-    except FileNotFoundError:
-        return HttpResponse("Email template file not found.")
+    notify_customers.delay("Hello")
+    return render(request,"main.html")
