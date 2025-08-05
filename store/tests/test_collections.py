91f680eed5d2c8afa6f3435b602ad52b56921f61
Kidus Petros <kidusptr@gmail.com>
2025-08-05 23:12:08 +0300

    more tests

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.test import APIClient
 import pytest
@@ -9,3 +10,15 @@ class TestCreateCollection:
         client = APIClient()
         response = client.post("/store/collections/", {"title": "a"})
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_returns_403(self):
+        client = APIClient()
+        client.force_authenticate(user={})
+        response = client.post("/store/collections/", {"title": "a"})
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_invalid_data_returns_400(self):
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+        response = client.post("/store/collections/", {"title": ""})
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
