2decd7b5685458e34d378477c08a4bd16820a707
Kidus Petros <kidusptr@gmail.com>
2025-08-10 22:37:45 +0300

    simulating slow api and getting baseline performance

--- locustfiles/browse_products.py
+++ locustfiles/browse_products.py
@@ -29,6 +29,10 @@ class WebsiteUser(HttpUser):
             json={"product_id": product_id, "quantity": 1},
         )
 
+    @task
+    def say_hello(self):
+        self.client.get("/playground/index/", name="/hello")
+
     def on_start(self):
         response = self.client.post("/store/carts/")
         print(response.status_code)
