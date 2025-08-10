d1976d8ae259b501c608be615aa6aa98c21f9bf7
Kidus Petros <kidusptr@gmail.com>
2025-08-10 22:50:09 +0300

    setting up redis for cache manager

--- mystore/settings.py
+++ mystore/settings.py
@@ -201,3 +201,13 @@ CELERY_BEAT_SCHEDULE = {
         "args": ["Hello Customer!"],
     }
 }
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/2",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    }
+}
