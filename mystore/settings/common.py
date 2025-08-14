555d2822c949df86c8fa1cee9f9b2fce61aa2d15
Kidus Petros <kidusptr@gmail.com>
2025-08-14 10:18:54 +0300

    managing development and production settings

--- mystore/settings/common.py
+++ mystore/settings/common.py
@@ -21,8 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-yh6o%&jdl96xe+kc8p#1bm22xfy21%qqk-=v4o_ivesm8e^20-"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ mystore/settings/dev.py
@@ -0,0 +1,5 @@
+from .common import *
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-yh6o%&jdl96xe+kc8p#1bm22xfy21%qqk-=v4o_ivesm8e^20-"
